712a0ea2be958a06d766e807b48da1735c9e3fa0
evil bocchi <treugames@gmail.com>
2025-07-07 21:06:47 +0800

    Update Automatic Railgun price and multiplier
    Increased the Bitcoin price from 1e9 to 5e9 and the Bitcoin multiplier from 4 to 5 for the Automatic Railgun item. This balances the item's cost and reward.

--- src/shared/items/0/automatic/AutomaticRailgun.ts
+++ src/shared/items/0/automatic/AutomaticRailgun.ts
@@ -9,7 +9,7 @@ export = new Item(script.Name)
     .setName("Automatic Railgun")
     .setDescription("Boom! Launches droplets to the skyline with a %mul% bang.")
     .setDifficulty(Difficulty.Automatic)
-    .setPrice(new CurrencyBundle().set("Funds", 20e39).set("Bitcoin", 1e9), 1)
+    .setPrice(new CurrencyBundle().set("Funds", 20e39).set("Bitcoin", 5e9), 1)
     .addPlaceableArea("BarrenIslands", "SlamoVillage", "SkyPavilion")
     .setCreator("simple13579")
 
@@ -18,6 +18,6 @@ export = new Item(script.Name)
 
     .trait(Upgrader)
     .setSky(true)
-    .setMul(new CurrencyBundle().set("Bitcoin", 4))
+    .setMul(new CurrencyBundle().set("Bitcoin", 5))
 
     .exit();
