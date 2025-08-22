f35f885602fbffe34f3f41965495c45661f50836
evil bocchi <71329833+evilbocchi@users.noreply.github.com>
2025-08-22 23:17:06 +0800

    feat: Add clearMostCurrencies and itemAll commands for server management

---
+++ src/server/services/permissions/commands/4/itemall.ts
@@ -0,0 +1,20 @@
+import Command, { CommandAPI } from "server/services/permissions/commands/Command";
+import Items from "shared/items/Items";
+
+export = new Command(script.Name)
+    .addAlias("ia")
+    .setDescription("Give 99 of all items into the inventory and place 1 of each item. Should only be done in sandbox.")
+    .setExecute(() => {
+        for (const [id, item] of Items.itemsPerId) {
+            CommandAPI.Item.setBoughtAmount(id, 0);
+            CommandAPI.Item.setItemAmount(id, 99);
+
+            const primaryPart = item.MODEL?.PrimaryPart;
+            if (primaryPart === undefined)
+                continue;
+
+            CommandAPI.Item.serverPlace(id, primaryPart.Position, 0);
+        }
+
+    })
+    .setPermissionLevel(4);
