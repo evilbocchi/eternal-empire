88f215568913e74bc744414a4bf5204b4121c95d
xy <221122309+Xydretonix@users.noreply.github.com>
2025-08-30 11:32:04 +0800

    update slopedropper.ts
    xy patch branch test 1
    
    Signed-off-by: xy <221122309+Xydretonix@users.noreply.github.com>

--- src/shared/items/1/walkaslope/SlopeDropper.ts
+++ src/shared/items/1/walkaslope/SlopeDropper.ts
@@ -1,1 +1,19 @@
+import Difficulty from "@antivivi/jjt-difficulties";
+import CurrencyBundle from "shared/currency/CurrencyBundle";
+import Droplet from "shared/item/Droplet";
+import Dropper from "shared/item/traits/dropper/Dropper";
+import Item from "shared/item/Item";
 
+export = new Item(script.Name)
+    .setName("Slope Dropper")
+    .setDescription("Produces droplets off a slope, weeeeee")
+    .setDifficulty(Difficulty.TheFirstDifficulty)
+    .setPrice(new CurrencyBundle().set("Funds", 640e44).set("Power", 145e27), 1)
+    .setPrice(new CurrencyBundle().set("Funds", 175e45).set("Power", 250e28), 2)
+    .addPlaceableArea("BarrenIslands","SkyPavillion")
+
+    .trait(Dropper)
+    .setDroplet(Droplet.TheFirstDroplet)
+    .setDropRate(0.75)
+
+    .exit();
