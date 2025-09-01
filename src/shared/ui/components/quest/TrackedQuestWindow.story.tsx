e727e3682bdf5268910d4e9fb31f3d66f67d5116
evil bocchi <71329833+evilbocchi@users.noreply.github.com>
2025-09-01 14:39:30 +0800

    Refactor quest UI and add TrackedQuestWindow story
    Refactored TrackedQuestWindow to simplify logic, update layout, and add gradient styling. Added a new story for TrackedQuestWindow. Improved QuestOption with animated dropdown arrow. Updated App.story to set a tracked quest for consistent storybook state. Replaced direct service access with imports in several components for consistency.

---
+++ src/shared/ui/components/quest/TrackedQuestWindow.story.tsx
@@ -0,0 +1,30 @@
+import React from "@rbxts/react";
+import ReactRoblox from "@rbxts/react-roblox";
+import { InferProps } from "@rbxts/ui-labs";
+import HotkeyProvider from "shared/ui/components/hotkeys/HotkeyProvider";
+import { questState } from "shared/ui/components/quest/QuestState";
+import TrackedQuestWindow from "shared/ui/components/quest/TrackedQuestWindow";
+import StoryMocking from "shared/ui/components/StoryMocking";
+import TooltipProvider from "shared/ui/components/tooltip/TooltipProvider";
+
+const controls = {
+    visible: true
+};
+
+export = {
+    react: React,
+    reactRoblox: ReactRoblox,
+    controls: controls,
+    story: (props: InferProps<typeof controls>) => {
+        StoryMocking.mockData();
+        questState.setTrackedQuest("Quest1");
+
+        return (
+            <HotkeyProvider>
+                <TooltipProvider>
+                    <TrackedQuestWindow />
+                </TooltipProvider>
+            </HotkeyProvider>
+        );
+    }
+};
