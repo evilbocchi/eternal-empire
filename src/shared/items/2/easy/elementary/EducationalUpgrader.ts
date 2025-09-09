9f59550c50322d71321cc9f74774de52d969e928
xy <221122309+Xydretonix@users.noreply.github.com>
2025-09-09 23:03:50 +0800

    redi
    Signed-off-by: xy <221122309+Xydretonix@users.noreply.github.com>

--- src/shared/items/2/easy/elementary/EducationalUpgrader.ts
+++ src/shared/items/2/easy/elementary/EducationalUpgrader.ts
@@ -7,12 +7,12 @@ import Upgrader from "shared/item/traits/upgrader/Upgrader";
 export = new Item(script.Name)
     .setName("EducationalUpgrader")
     .setDescription("Give your droplets an education that provides a MASSIVE boost by %val% to your droplets!  ")
-    .setDifficulty(Difficulty.Educational)
+    .setDifficulty(Difficulty.Material)
     .setPrice(new CurrencyBundle().set("Funds", 15e52).set("Power", 1e33), 1)
-    .addPlaceableArea("BarrenIslands", "SkyPavillion")
+    .addPlaceableArea("BarrenIslands", "SkyPavilion")
 
     .trait(Upgrader)
-    .setAdd(new CurrencyBundle().setMul("Funds", 15).setMul("Power", 8.75).setMul("Bitcoin",  4.5).setMul("Skill", 3.14))
+    .setMul(new CurrencyBundle().set("Funds", 15).set("Power", 8.75).set("Bitcoin",  4.5).set("Skill", 3.14))
 
     .trait(Conveyor)
     .setSpeed(1)
