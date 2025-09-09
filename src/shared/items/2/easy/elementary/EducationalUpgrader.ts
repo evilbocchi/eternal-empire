ee147d84051981b7b07864a4349ed6382ba73c05
xy <221122309+Xydretonix@users.noreply.github.com>
2025-09-09 23:01:32 +0800

    create EducationalUpgrader.ts
    Signed-off-by: xy <221122309+Xydretonix@users.noreply.github.com>

--- src/shared/items/2/easy/elementary/EducationalUpgrader.ts
+++ src/shared/items/2/easy/elementary/EducationalUpgrader.ts
@@ -1,1 +1,20 @@
+import Difficulty from "@antivivi/jjt-difficulties";
+import CurrencyBundle from "shared/currency/CurrencyBundle";
+import Conveyor from "shared/item/traits/conveyor/Conveyor";
+import Item from "shared/item/Item";
+import Upgrader from "shared/item/traits/upgrader/Upgrader";
 
+export = new Item(script.Name)
+    .setName("EducationalUpgrader")
+    .setDescription("Give your droplets an education that provides a MASSIVE boost by %val% to your droplets!  ")
+    .setDifficulty(Difficulty.Educational)
+    .setPrice(new CurrencyBundle().set("Funds", 15e52).set("Power", 1e33), 1)
+    .addPlaceableArea("BarrenIslands", "SkyPavillion")
+
+    .trait(Upgrader)
+    .setAdd(new CurrencyBundle().setMul("Funds", 15).setMul("Power", 8.75).setMul("Bitcoin",  4.5).setMul("Skill", 3.14))
+
+    .trait(Conveyor)
+    .setSpeed(1)
+
+    .exit();
