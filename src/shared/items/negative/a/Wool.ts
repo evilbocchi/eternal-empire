c05c25a1aafb008153822c22bcdeadefd02b2d32
evil bocchi <71329833+evilbocchi@users.noreply.github.com>
2025-10-04 15:50:37 +0800

    Fix Wool difficulty

--- src/shared/items/negative/a/Wool.ts
+++ src/shared/items/negative/a/Wool.ts
@@ -5,7 +5,7 @@ import Item from "shared/item/Item";
 export = new Item(script.Name)
     .setName("Wool")
     .setDescription("Wool for the masses.")
-    .setDifficulty(Difficulty.A)
+    .setDifficulty(Difficulty.TrueEase)
     .setPrice(new CurrencyBundle().set("Funds", 24e12).set("Power", 100))
     .placeableEverywhere()
     .persists();
