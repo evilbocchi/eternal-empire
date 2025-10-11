a87f2a2a4bf68e2d6842aa898b7ae445e3363323
LeopolisDev <leopolis730@gmail.com>
2025-10-11 12:19:02 +0300

    works good

--- src/shared/items/negative/instantwin/PurifiersDream.ts
+++ src/shared/items/negative/instantwin/PurifiersDream.ts
@@ -44,7 +44,7 @@ export = new Item(script.Name)
                                 if (data !== undefined) {
                                     const newRawClicks = ++data.rawPurifierClicks;
                                     Packets.rawPurifierClicks.setFor(player, newRawClicks);
-                                    CurrencyService.incrementAll(_result.amountPerCurrency);
+                                    CurrencyService.increment("Purifier Clicks", new OnoeNum(400));
                                     CurrencyService.propagate();
                                 }
                             };
