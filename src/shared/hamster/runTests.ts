1afe213b7509e28ede2585cad2a61b9451fe0054
evil bocchi <71329833+evilbocchi@users.noreply.github.com>
2025-11-11 17:13:39 +0800

    Always set Fletchette virtual state in runTests
    Removed conditional check for IS_EDIT and now always set FletchetteEnvironment to virtual state when running tests. Simplifies test environment setup.

--- src/shared/hamster/runTests.ts
+++ src/shared/hamster/runTests.ts
@@ -1,15 +1,10 @@
 import { FletchetteEnvironment } from "@rbxts/fletchette";
 import { runCLI } from "@rbxts/jest";
 import { ServerScriptService } from "@rbxts/services";
-import { IS_EDIT } from "shared/Context";
 
 export = () => {
     const root = ServerScriptService.WaitForChild("tests");
-
-    if (IS_EDIT) {
-        FletchetteEnvironment.setVirtualState(true);
-    }
-
+    FletchetteEnvironment.setVirtualState(true);
     const [success, output] = runCLI(
         root,
         {
@@ -17,6 +12,5 @@ export = () => {
         },
         [root],
     ).await();
-
     return $tuple(success, output);
 };
